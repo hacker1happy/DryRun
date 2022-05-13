a14a081cdb0f2e62e739912f07fd5b953262715e
hacker1happy <9547priyanshu.gupta@gmail.com>
2022-05-13 16:51:34 +0530

    bubble sort started

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const app = express();
 
 app.set('views engine','ejs');
 app.use(express.static('public'))
+
 app.get('/', async(req,res)=>{
-   const link ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtT7VWUnR48hinUbRQMl5yu2EqYH6nIB3pDQ&usqp=CAU"
-   res.render('index.ejs',{link});
-   
+   res.render('index.ejs'); 
 })
 app.get('/home', async(req,res)=>{
     res.render('index.ejs');      
    })
-app.get('/about', async(req,res)=>{
-    res.render('about.ejs');      
-   })
+// app.get('/about', async(req,res)=>{
+//     res.render('about.ejs');      
+//    })
 
 app.get('/bubble', async(req,res)=>{
     res.render('bubble.ejs');
+      
+   })
+app.get('/result', async(req,res)=>{
+    res.render('result.ejs');
       
    })
 app.listen(3000,()=>{
---
+++ public/js/result.js
@@ -0,0 +1,43 @@
+var lst = localStorage.getItem("elements").split(",");;
+
+function print(lst, j) {
+  var htmlData = "<table><tr>"
+  for (i = 0; i < lst.length; i++) {
+    if (i == j) {
+      htmlData = htmlData + "<td style=\"background-color:white;\">" + lst[i] + "</td>"
+    } else if (i === j + 1) {
+      htmlData = htmlData + "<td style=\"background-color:grey;\">" + lst[i] + "</td>"
+    } else {
+      htmlData = htmlData + "<td>" + lst[i] + "</td>"
+    }
+  }
+  htmlData = htmlData + "</tr></table>"
+  document.getElementById("example").innerHTML = htmlData
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function bblSort(arr) {
+
+
+  for (var i = 0; i < arr.length-1; i++) {
+
+    for (var j = 0; j < (arr.length - i -1); j++) {
+
+      print(arr, j);
+      await sleep(2000);
+
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+      print(arr, j);
+      await sleep(2000);
+    }
+  }
+}
+
+bblSort(lst);
